910019c17edeb2d7b26f67a51704527d2f51ec86
Titus Waldner <Titus.Waldner@protonmail.com>
2026-08-09 11:55:18 -0500

    Add input event model and ignore build output

---
+++ include/InputEvent.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+
+enum class EventType
+ {
+     MouseMove,
+     MouseButtonDown,
+     MouseButtonUp,
+     MouseWheel,
+     KeyDown,
+     KeyUp
+ };
+
+struct InputEvent
+ {
+     std::uint64_t timestampMicroseconds = 0;
+     EventType type = EventType::MouseMove;
+
+    int mouseX = 0;
+     int mouseY = 0;
+     int mouseDeltaX = 0;
+     int mouseDeltaY = 0;
+     int mouseButton = 0;
+     int mouseWheelDelta = 0;
+
+    unsigned int keyCode = 0;
+ };
